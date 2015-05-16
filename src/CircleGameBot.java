08f169cfc059b70051acf114b5881b6b5cd0288b
Matthew Pearce <pearcematthew25@gmail.com>
2015-05-16 19:53:10 -0400

    fgdfsdgdh

--- src/CircleGameBot.java
+++ src/CircleGameBot.java
@@ -1,54 +1,24 @@
 import java.awt.*;
 import java.awt.event.*;
 
-public class CircleGameBot implements MouseListener{
+public class CircleGameBot {
 
+	public static void main(String[] args){
+		
 	
-	public static void main(String[] args) throws AWTException{
-		Robot bot = new Robot();
 		
-		
-	}
-	
-	public static void printPixelColor(int x, int y) throws AWTException{
-		
-		Robot bot = new Robot();
-		System.out.println(bot.getPixelColor(x,y));
-	}
-
-
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		try {
-			printPixelColor(e.getX(),e.getY());
-		} catch (AWTException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+		for (int i = 0; i < 100; i ++){
+			
+			
+			System.out.println(getColor());
 		}
-		
 	}
-
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
+	
+	public static String getColor(){
+		Robot bot = new Robot();
 		
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		System.out.println("Hey");
-		
+		String color = "";
+		Mouse
+		return color;
 	}
 }
